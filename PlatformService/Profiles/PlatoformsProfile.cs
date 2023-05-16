70a28479e3fc4fc62d84351faa464875ee27dbc2
João Vitor <joaovitorjlle2001@gmail.com>
2023-05-16 17:13:02 -0300

    Mapper and Dtos

---
+++ PlatformService/Profiles/PlatoformsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatoformsProfile : Profile
+    {
+        public PlatoformsProfile()
+        {
+            //Source - Target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatforCreateDto, Platform>();
+        }
+    }
+}
